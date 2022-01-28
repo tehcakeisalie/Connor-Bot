a9bfd36a03676a16f78c47943d29839c3c39cced
cjmac <cjmac@DESKTOP-S4MIDD4.local>
2022-01-28 15:49:33 -0800

    added new headline and organized the headline # by date

--- Connor Bot/Program.cs
+++ Connor Bot/Program.cs
@@ -111,7 +111,7 @@ namespace Connor_Bot
 
             //the help command. It gives you a list of Connor bot's main functions.
             if (command.Equals("c!help"))
-                message.Channel.SendMessageAsync("1. c!philisophy - hear some mind-blowing philosophy\n2. c!fortune - get your fortune told\n3. c!8ball - ask the magic 8-ball a yes or no question\n4. c!threat - I will threaten you.\n5. c!headline - get the latest news!\n6. c!headline #(number here) - pick a specidic headline. there are currently 13.\n7.c!predictme - I'll try to predict what kind of Discord user you are.\n8. c!opponents - face off against challengers or even other users in a battle of complete random chance!\nremember, there's a few secret text imputs that can have varying responses!\nalso remember, my creator won't have his computer on all the time, so check to see if I'm online before entering a command\nIf my status says *under maintinence*, I am just that: under maintinence and only online for testing.\nNow have fun with the shitpost of a bot I am!");
+                message.Channel.SendMessageAsync("1. c!philisophy - hear some mind-blowing philosophy\n2. c!fortune - get your fortune told\n3. c!8ball - ask the magic 8-ball a yes or no question\n4. c!threat - I will threaten you.\n5. c!headline - get the latest news!\n6. c!headline #(number here) - pick a specidic headline. there are currently 14.\n7.c!predictme - I'll try to predict what kind of Discord user you are.\n8. c!opponents - face off against challengers or even other users in a battle of complete random chance!\nremember, there's a few secret text imputs that can have varying responses!\nalso remember, my creator won't have his computer on all the time, so check to see if I'm online before entering a command\nIf my status says *under maintinence*, I am just that: under maintinence and only online for testing.\nNow have fun with the shitpost of a bot I am!");
 
             //the fist line I ever wrote. responds to !hello
             if (command.Equals("c!hello"))
@@ -832,7 +832,7 @@ namespace Connor_Bot
 
             //gives some breaking news
             Random News = new Random();
-            int randomNews = News.Next(1, 13);
+            int randomNews = News.Next(1, 14);
             if (command.Equals("c!headline"))
                 switch (randomNews)
                 {
@@ -889,10 +889,14 @@ namespace Connor_Bot
                         message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2003 Hardware war begins.jpg");
                         break;
                     case 14:
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1948 Rise of Bot incorporated.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1948 Rise of Bot incorporated.jpg");
+                        break;
+                    case 15:
                         message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/");
                         message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/");
                         break;
-                    case 15:
+                    case 16:
                         message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/");
                         message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/");
                         break;
@@ -906,62 +910,66 @@ namespace Connor_Bot
                 switch (numberfornews)
                 {
                     case 1:
-                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2006 man drunk die.jpg");
-                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2006 man drunk die.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1948 Discovery of Connor bot.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1948 Discovery of Connor bot.jpg");
                         break;
                     case 2:
-                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1963 KOTC bribery scandal.jpg");
-                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1963 KOTC bribery scandal.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1948 Rise of Bot incorporated.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1948 Rise of Bot incorporated.jpg");
                         break;
                     case 3:
-                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2020 brain transplant.jpg");
-                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2020 brain transplant.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1963 KOTC bribery scandal.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1963 KOTC bribery scandal.jpg");
                         break;
                     case 4:
                         message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1987 Ohio.jpg");
                         message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1987 Ohio.jpg");
                         break;
                     case 5:
-                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2060 Ikea is hell.jpg");
-                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2060 Ikea is hell.jpg");
-                        break;
-                    case 6:
                         message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1990 Disney got BIG.jpg");
                         message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1990 Disney got BIG.jpg");
                         break;
-                    case 7:
-                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2021 Baskin Robbins exposed.jpg");
-                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2021 Baskin Robbins exposed.jpg");
-                        break;
-                    case 8:
-                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2021 couch on the rise.jpg");
-                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2021 couch on the rise.jpg");
-                        break;
-                    case 9:
-                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2041 A war breaks out.jpg");
-                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2041 A war breaks out.jpg");
-                        break;
-                    case 10:
-                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1996 alcohol is banned.jpg");
-                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1996 alcohol is banned.jpg");
-                        break;
-                    case 11:
+                    case 6:
                         message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1991 the CBOM is concerning.jpg");
                         message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1991 the CBOM is concerning.jpg");
                         break;
-                    case 12:
-                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1948 Discovery of Connor bot.jpg");
-                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1948 Discovery of Connor bot.jpg");
+                    case 7:
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/1996 alcohol is banned.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/1996 alcohol is banned.jpg");
                         break;
-                    case 13:
+                    case 8:
                         message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2003 Hardware war begins.jpg");
                         message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2003 Hardware war begins.jpg");
                         break;
+                    case 9:
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2006 man drunk die.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2006 man drunk die.jpg");
+                        break;
+                    case 10:
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2020 brain transplant.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2020 brain transplant.jpg");
+                        break;
+                    case 11:
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2021 Baskin Robbins exposed.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2021 Baskin Robbins exposed.jpg");
+                        break;
+                    case 12:
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2021 couch on the rise.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2021 couch on the rise.jpg");
+                        break;
+                    case 13:
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2041 A war breaks out.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2041 A war breaks out.jpg");
+                        break;
                     case 14:
+                        message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/2060 Ikea is hell.jpg");
+                        message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/2060 Ikea is hell.jpg");
+                        break;
+                    case 15:
                         message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/");
                         message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/");
                         break;
-                    case 15:
+                    case 16:
                         message.Channel.SendFileAsync($@"C:/Users/Connor/OneDrive/Pictures/Connor Bot/");
                         message.Channel.SendFileAsync($@"C:/Users/cjmac/OneDrive/Pictures/Connor Bot/");
                         break;
